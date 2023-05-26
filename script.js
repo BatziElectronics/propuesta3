d3dadb314d6d22d99feb297b5ac9f5e90f9a3e0a
Axel_Electronica <103341896+BatziElectronics@users.noreply.github.com>
2023-05-25 21:43:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function cambiarTexto() {
+var texto=document.getElementById ("texto");
+var texto3=document.getElementById ("texto3");
+var boton=document.getElementById ("boton");
+texto.innerHTML= "Jaja solo acepta!!";
+boton.style.display= "none";
+texto3.style.display= "none";
+}
